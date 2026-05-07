34c8128b01ee8760e640d51ad2532dc331983147
Your Name <your-email@example.com>
2026-05-07 01:30:28 -0700

    add section two in landing page

--- frontend/jamin/src/landing/LandingPage/LandingPage.jsx
+++ frontend/jamin/src/landing/LandingPage/LandingPage.jsx
@@ -11,6 +11,32 @@ const LandingPage = () => {
         <button>Get Started <i class="fa-solid fa-arrow-right"></i></button>
 
       </div>
+      <section>
+        <div className="landingpage-section-two">
+          <div className="section-two-heading">
+            <span>Our Workflow</span>
+            <h1>How it Works</h1>
+          </div>
+          <div className="section-two-boxes">
+            <div className="SearchPrecisely">
+              <i class="fa-solid fa-earth-americas"></i>
+              <h2>Search Precisely</h2>
+              <p>Find your perfect plot by filtering through District, Block, and specific Village records with pinpoint accuracy.</p>
+            </div>
+            <div className="VerifyLegally">
+              <i class="fa-regular fa-file-lines"></i>
+              <h2>Verify Legally</h2>
+              <p>Every listing is linked to official Jamabandi and Khatian records ensuring 100% legal security.</p>
+            </div>
+            <div className="ContactDirectly">
+              <i class="fa-regular fa-message"></i>
+              <h2>Contact Directly</h2>
+              <p>Connect with sellers directly and get your queries resolved in real-time.</p>
+            </div>
+          </div>
+        </div>
+
+      </section>
     </div>
   )
 }
